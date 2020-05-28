c0116f009c7ee228f6725e1550cc822f92b73497
João Reis <jaareis16@gmail.com>
2020-05-28 19:08:06 +0100

    profile

--- app/src/main/java/com/example/bydelivery_app/ProfileFragment.java
+++ app/src/main/java/com/example/bydelivery_app/ProfileFragment.java
@@ -1,22 +1,20 @@
 package com.example.bydelivery_app;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-public class HomeFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
-
-
-
 }
+
+
