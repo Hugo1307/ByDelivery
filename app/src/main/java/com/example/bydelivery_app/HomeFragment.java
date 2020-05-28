da727099d3d579afc2d4c0624d171663c215bcb9
João Reis <jaareis16@gmail.com>
2020-05-28 19:12:24 +0100

    .

--- app/src/main/java/com/example/bydelivery_app/HomeFragment.java
+++ app/src/main/java/com/example/bydelivery_app/HomeFragment.java
@@ -13,7 +13,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 
